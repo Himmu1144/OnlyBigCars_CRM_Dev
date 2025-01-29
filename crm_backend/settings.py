7cfe5023c0e1ba20f111e27cf80d372d2adb0e8a
OnlyBigCars <gokulpatil012@gmail.com>
2025-01-29 14:28:22 +0530

    Update settings.py

--- crm_backend/settings.py
+++ crm_backend/settings.py
@@ -157,7 +157,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
